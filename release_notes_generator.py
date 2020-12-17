8f05b82d5187713486828f52ed83bf6d73d0926d
Katie Frey <katieefrey@gmail.com>
2020-12-17 10:51:02 -0500

    updated release notes generator

--- release_notes_generator.py
+++ release_notes_generator.py
@@ -10,54 +10,31 @@ import json
 import codecs
 import shutil
 import rdflib
-import cStringIO
 import unicodedata
 #import pandas as pd
 from datetime import datetime
 
 timestamp = datetime.now().strftime("%Y_%m%d_%H%M")
 
-#UnicodeWriter from http://docs.python.org/2/library/csv.html#examples
-class UnicodeWriter:
-    def __init__(self, f, dialect=csv.excel, encoding="utf-8-sig", **kwds):
-        self.queue = cStringIO.StringIO()
-        self.writer = csv.writer(self.queue, dialect=dialect, **kwds)
-        self.stream = f
-        self.encoder = codecs.getincrementalencoder(encoding)()
-    def writerow(self, row):
-        self.writer.writerow([s.encode("utf-8") for s in row])
-        data = self.queue.getvalue()
-        data = data.decode("utf-8")
-        data = self.encoder.encode(data)
-        self.stream.write(data)
-        self.queue.truncate(0)
-
-    def writerows(self, rows):
-        for row in rows:
-            self.writerow(row)
-
-#/end UnicodeWriter
-
-print "Reading the SKOS file...this may take a few seconds."
-
+print ("Reading the SKOS file...this may take a few seconds.")
 ##### RDF File Location #####
 ##### assign this variable to location of UAT SKOS-RDF file exported from VocBench ##### 
 
 ##export RDF/XML Concepts
-uat_new = "UAT3.1.rdf" # filename for the new version
+uat_new = "UAT.rdf" # filename for the new version
 
 #get previous version RDF from GitHub
-uat_prev = "UAT3.0.rdf" # filename for the previous version
+uat_prev = "uat-old.rdf" # filename for the previous version
 
 ##### Shared Functions and Variables #####
 ##### do NOT edit this section #####
 
 #reads SKOS-RDF file into a RDFlib graph for use in these scripts
 g = rdflib.Graph()
-result = g.parse((uat_new).encode('utf8'))
+result = g.parse(uat_new)#.encode('utf8'))
 
 f = rdflib.Graph()
-result = f.parse((uat_prev).encode('utf8'))
+result = f.parse(uat_prev)#.encode('utf8'))
 
 #defines certain properties within the SKOS-RDF file
 prefLabel = rdflib.term.URIRef('http://www.w3.org/2004/02/skos/core#prefLabel')
@@ -69,10 +46,12 @@ TopConcept = rdflib.term.URIRef('http://www.w3.org/2004/02/skos/core#topConceptO
 ednotes = rdflib.term.URIRef('http://www.w3.org/2004/02/skos/core#editorialNote')
 changenotes = rdflib.term.URIRef('http://www.w3.org/2004/02/skos/core#changeNote')
 scopenotes = rdflib.term.URIRef('http://www.w3.org/2004/02/skos/core#scopeNote')
-termdef = rdflib.term.URIRef('http://www.w3.org/2004/02/skos/core#definition')
 example = rdflib.term.URIRef('http://www.w3.org/2004/02/skos/core#example')
 related = rdflib.term.URIRef('http://www.w3.org/2004/02/skos/core#related')
-
+definition = rdflib.term.URIRef('http://www.w3.org/2004/02/skos/core#definition')
+comment = rdflib.term.URIRef('http://www.w3.org/2000/01/rdf-schema#comment')
+title = rdflib.term.URIRef('http://purl.org/dc/terms/title')
+label = rdflib.term.URIRef('http://www.w3.org/2000/01/rdf-schema#label')
 
 #a list of all concepts
 allnewconcepts = [gm for gm in g.subjects(rdflib.RDF.type, Concept)]
@@ -134,84 +113,82 @@ def getbroaderterms(term,version):
 
 
 #a function to return scope notes for a term
-def getscopenotes(term):
+def getscopenotes(term,sf):
     d = rdflib.term.URIRef(term)
-    for scnoteterm in g.objects(subject=d, predicate=scopenotes):
+    for scnoteterm in sf.objects(subject=d, predicate=scopenotes):
         return scnoteterm
 
 #a function to return example notes for a term
-def getexample(term):
+def getexample(term,sf):
     d = rdflib.term.URIRef(term)
-    for termex in g.objects(subject=d, predicate=example):
-        return termex
+    exlist = []
+    for termex in sf.objects(subject=d, predicate=example):
+        exlist.append(termex)
+    return exlist
 
 #a function to return the status of a term    
-def getdefinition(term):
+def getdefinition(term,sf):
     d=rdflib.term.URIRef(term)
-    for deftest in g.objects(subject=d, predicate=termdef):
+    for deftest in sf.objects(subject=d, predicate=definition):
         return deftest
 
 
-fileout = codecs.open('changes_'+timestamp+'.csv', 'wb')
+fileout = open('changes_'+timestamp+'.csv','w', encoding='utf-8', newline='')
 
 csv_out = csv.writer(fileout, lineterminator='\n', delimiter=',')
-wr = UnicodeWriter(fileout,lineterminator='\n', delimiter=',', dialect='excel',quoting=csv.QUOTE_ALL)
+wr = csv.writer(fileout,quoting=csv.QUOTE_ALL)#
+#UnicodeWriter(fileout,lineterminator='\n', delimiter=',', dialect='excel',quoting=csv.QUOTE_ALL)
 
 ##prints all new concepts, new alts, removed alts
 
 for newcon in allnewconcepts:
     if newcon in allprevconcepts:
-
         newalts = getaltterms(newcon, g)
         oldalts = getaltterms(newcon, f)
 
-        if oldalts == None and newalts == None :
+        copynewalts = getaltterms(newcon, g)
+        copyoldalts = getaltterms(newcon, f)
+
+        if oldalts == None or newalts == None :
             pass
-        elif oldalts == None and newalts != None:
 
-            anewalts = (", ").join(newalts)
-
-            wr.writerow((["New Alts"]+[newcon[30:]]+["| "]+[newcon]+[" | "]+[lit(newcon)]+[" | "]+[anewalts.encode("utf-8")]+[" |"]))
-        elif oldalts != None and newalts == None:
-            wr.writerow((["Removed Alts"]+[newcon[30:]]+["| "]+[newcon]+[" | "]+[lit(newcon)]+[" | "]+[aoldalts.encode("utf-8")]+[" |"]))
-        elif oldalts != None and newalts != None:
-
-            aoldalts = (", ").join(oldalts)
-
-            altlist = []
+        else:
             for x in newalts:
                 if x in oldalts:
-                    pass
-                else:
-                    altlist.append(x)
+                    copynewalts.remove(x)
 
-            aaltlist = (", ").join(altlist)
-            if altlist != []:
-                wr.writerow((["New Alts"]+[newcon[30:]]+["| "]+[newcon]+[" | "]+[lit(newcon)]+[" | "]+[aaltlist.encode("utf-8")]+[" |"]))
-
-            depaltlist = []
             for y in oldalts:
                 if y in newalts:
-                    pass
-                else:
-                    depaltlist.append(y)
-            if depaltlist != []:
-                for z in depaltlist:
-                    if z == lit(newcon):
-                        pass
-                    else:
-                        wr.writerow((["Removed Alts"]+[newcon[30:]]+["| "]+[newcon]+[" | "]+[lit(newcon)]+[" | "]+depaltlist+[" |"]))
+                    copyoldalts.remove(y)
+
+        if copyoldalts != None and copyoldalts != []:
+            aoldalts = (", ").join(copyoldalts)
+            wr.writerow((["Removed Alts"]+[newcon[30:]]+["| "]+[newcon]+[" | "]+[lit(newcon)]+[" | "]+[aoldalts]+[" |"]))
+
+        if copynewalts != None and copynewalts != []:
+            anewalts = (", ").join(copynewalts)
+            wr.writerow((["New Alts"]+[newcon[30:]]+["| "]+[newcon]+[" | "]+[lit(newcon)]+[" | "]+[anewalts]+[" |"]))
+
+    #         depaltlist = []
+    #         for y in oldalts:
+    #             if y in newalts:
+    #                 pass
+    #             else:
+    #                 depaltlist.append(y)
+    #         if depaltlist != []:
+    #             for z in depaltlist:
+    #                 if z == lit(newcon):
+    #                     pass
+    #                 else:
+    #                     wr.writerow((["Removed Alts"]+[newcon[30:]]+["| "]+[newcon]+[" | "]+[lit(newcon)]+[" | "]+depaltlist+[" |"]))
     else:
         litterm = lit(newcon)        
         morealts = getaltterms(newcon, g)           
 
-        wr.writerow(("New concept",newcon[30:],"| ",newcon," | ",litterm.encode("utf-8")," |"))
+        wr.writerow(("New concept",newcon[30:],"| ",newcon," | ",litterm," |"))
         if morealts != None:
             amorealts = (", ").join(morealts)
             wr.writerow((["New Alts"]+[newcon[30:]]+["| "]+[newcon]+[" | "]+[lit(newcon)]+[" | "]+[amorealts]+[" |"]))
-        else:
-            pass
-
 
 ##finds all deprecated concepts
 for oldcon in allprevconcepts:
@@ -219,15 +196,10 @@ for oldcon in allprevconcepts:
         oldlit = deplit(oldcon)
         newlit = lit(oldcon)
         if oldlit != newlit:
-            #print oldlit
-            #print newlit
-            wr.writerow(("Updated PrefLabel",oldcon[30:],"| ",oldcon.encode("utf-8")," | ",oldlit.encode("utf-8")," | ",newlit.encode("utf-8")," |"))
-        else:
-            pass
-
+            wr.writerow(("Updated PrefLabel",oldcon[30:],"| ",oldcon," | ",oldlit," | ",newlit," |"))
     else:
         litterm = deplit(oldcon)
-        wr.writerow(("Deprecated concept",oldcon[30:],"| ",oldcon," | ",litterm.encode("utf-8")," |"))
+        wr.writerow(("Deprecated concept",oldcon[30:],"| ",oldcon," | ",litterm," |"))
 
 
 #finds all new related links
@@ -236,32 +208,23 @@ relatedlist = []
 for oldcon in allprevconcepts:
     litterm = lit(oldcon)
     rterms = getrelatedterms(oldcon,f)
-    if rterms == None:
-
-        pass
-    else:
+    if rterms != None:
         for x in rterms:
             littermx = lit(x)
-            relatedlist.append([oldcon.encode("utf-8"),x.encode("utf-8")])
-            #csv_out.writerow(("old related",oldcon.encode("utf-8"),litterm.encode("utf-8"),x.encode("utf-8"),littermx.encode("utf-8")))
-
+            relatedlist.append([oldcon,x])
 
 newrelatedlist = []
 for newcon in allnewconcepts:
     litterm = lit(newcon)
-    #print "concept: "+ newcon
     rterms = getrelatedterms(newcon,g)
-    if rterms == None:
-
-        pass
-    else:
+    if rterms != None:
         for x in rterms:
             littermx = lit(x)
-            newrelatedlist.append([newcon.encode("utf-8"),x.encode("utf-8")])
-            if [newcon.encode("utf-8"),x.encode("utf-8")] in relatedlist:
+            newrelatedlist.append([newcon,x])
+            if [newcon,x] in relatedlist:
                 pass
             else:
-                csv_out.writerow(("Related",newcon[30:],"| ",newcon.encode("utf-8")," |",litterm.encode("utf-8")," | ",x.encode("utf-8")," | ",littermx.encode("utf-8")," |"))
+                wr.writerow(("Related",newcon[30:],"| ",newcon," |",litterm," | ",x," | ",littermx," |"))
 
 
 
@@ -270,55 +233,44 @@ deflist = []
 scopelist = []
 examplelist = []
 for oldcon in allprevconcepts:
-    olddef = getdefinition(oldcon)
-    oldscope = getscopenotes(oldcon)
-    oldex = getexample(oldcon)
+    olddef = getdefinition(oldcon,f)
+    oldscope = getscopenotes(oldcon,f)
+    oldex = getexample(oldcon,f)
 
-    if olddef == None:
-        pass
-    else:
-        deflist.append([oldcon.encode("utf-8"),olddef.encode("utf-8")])
+    if olddef != None:
+        deflist.append([oldcon,olddef])
 
-    if oldscope == None:
-        pass
-    else:
-        scopelist.append([oldcon.encode("utf-8"),oldscope.encode("utf-8")])
+    if oldscope != None:
+        scopelist.append([oldcon,oldscope])
 
-    if oldex == None:
-        pass
-    else:
-        examplelist.append([oldcon.encode("utf-8"),oldex.encode("utf-8")])
+    if oldex != None:
+        examplelist.append([oldcon,oldex])
 
 
 for newcon in allnewconcepts:
-    newdef = getdefinition(newcon)
-    newscope = getscopenotes(newcon)
-    newex = getexample(newcon)
+    newdef = getdefinition(newcon,g)
+    newscope = getscopenotes(newcon,g)
+    newex = getexample(newcon,g)
     litterm = lit(newcon)
 
-    if newdef == None:
-        pass
-    else:
-        if [newcon.encode("utf-8"),newdef.encode("utf-8")] in deflist:
+    if newdef != None:
+        if [newcon,newdef] in deflist:
             pass
         else:
-            csv_out.writerow(("Definition",newcon[30:],"| ",newcon.encode("utf-8")," |",litterm.encode("utf-8")," |",newdef.encode("utf-8")," |"))
+            wr.writerow(("Definition",newcon[30:],"| ",newcon," |",litterm," |",newdef," |"))
 
-    if newscope == None:
-        pass
-    else:
-        if [newcon.encode("utf-8"),newscope.encode("utf-8")] in scopelist:
+    if newscope != None:
+        if [newcon,newscope] in scopelist:
             pass
         else:
-            csv_out.writerow(("Scope Note",newcon[30:],"| ",newcon.encode("utf-8")," |",litterm.encode("utf-8")," |",newscope.encode("utf-8")," |"))
+            wr.writerow(("Scope Note",newcon[30:],"| ",newcon," |",litterm," |",newscope," |"))
 
-    if newex == None:
-        pass
-    else:
-        if [newcon.encode("utf-8"),newex.encode("utf-8")] in examplelist:
+    if newex != []:
+        if [newcon,newex] in examplelist:
             pass
         else:
-            csv_out.writerow(("Example",newcon[30:],"| ",newcon.encode("utf-8")," |",litterm.encode("utf-8")," |",newex.encode("utf-8")," |"))
+            nex = ", ".join(newex)
+            wr.writerow(("Example",newcon[30:],"| ",newcon," |",litterm," |",nex," |"))
 
 
 #gets removed related links
@@ -326,8 +278,8 @@ for a in relatedlist:
     if a in newrelatedlist:
         pass
     else:
-        csv_out.writerow(("Removed Related",a[0][30:],"| ",a[0].encode("utf-8")," |",deplit(a[0]).encode("utf-8")," |",a[1].encode("utf-8")," |",deplit(a[1]).encode("utf-8")," |"))
+        wr.writerow(("Removed Related",a[0][30:],"| ",a[0]," |",deplit(a[0])," |",a[1]," |",deplit(a[1])," |"))
 
 fileout.close()
 
-print "finished!"
+print ("finished!")
